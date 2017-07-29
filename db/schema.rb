5f6121e4ab21c61ce06a088103953984a15135ef
Nicole Shayne <nicoleshayne.elem@gmail.com>
2017-07-29 18:45:10 +0800

    New changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727043555) do
+ActiveRecord::Schema.define(version: 20170728095849) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170727043555) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text "bio"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
